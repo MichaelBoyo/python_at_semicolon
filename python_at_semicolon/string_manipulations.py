b28a2b4ad6aa1303f39e4c989727fb97b30e6847
MichaelBoyo <103725264+MichaelBoyo@users.noreply.github.com>
2022-06-06 14:39:24 +0100

    m

--- python_at_semicolon/string_manipulations.py
+++ python_at_semicolon/string_manipulations.py
@@ -1,2 +1,17 @@
-s = "hello"
-print(s.find("l", 1, 4))
+
+s = "hello world"
+print(s.find("o", 1, 3))
+print(s.index("l"))
+print(s.isalpha())
+print(s.isdigit())
+print(s.capitalize())
+print(s.startswith("h"))
+print(s.endswith("o"))
+print(s.split())
+print(s.removeprefix("hello "))
+print(s.swapcase())
+
+tct = "34"
+print(tct.zfill(5))
+print(ord("r"))
+print("l" in s)
